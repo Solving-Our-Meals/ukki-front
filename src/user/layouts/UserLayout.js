311d348af047eac6edf2318d6482bc1aa795cf3c
DESKTOP-43RUNA1\USER <rhkdlf48301@gmail.com>
2024-12-24 08:45:29 +0900

    feature :storedetail/2024-12-24

--- src/user/layouts/UserLayout.js
+++ src/user/layouts/UserLayout.js
@@ -2,18 +2,33 @@ import { Outlet } from "react-router-dom";
 import Header from "../../common/header/components/Header";
 import FloatingBar from "../../common/floatingBar/components/FloatingBar";
 import Footer from "../../common/footer/components/Footer";
-import styles from '../layouts/css/userlayout.module.css';
+import styles from './css/userlayout.module.css';
+import { useEffect } from "react";
 
 function UserLayout() {
+
+    useEffect(() => {
+        
+    var parentElement = document.getElementById('parent');
+    var childElement = document.getElementById('child');
+    var childHeight = childElement.clientHeight;
+
+    parentElement.style.height = childHeight + 'px';
+
+    }, []);
+
     return (
-        <div className={styles.layoutStyle}>
+        <div className={styles.wrapper}>
             <Header className={styles.header} />
-            <main className={styles.main}>
-                <FloatingBar />
-                <Outlet />
+            <main id="parent" className={styles.contentWrapper}>
+                <div id="child">
+                    <FloatingBar />
+                    <Outlet />
+                </div>
             </main>
             <Footer className={styles.footer} />
         </div>
+        
     );
 }
 
