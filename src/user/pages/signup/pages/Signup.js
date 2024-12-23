b58b5ca143bfb389490a3d7f4b2b68c86a9c58f9
Pear1yCode <gh_wns@naver.com>
2024-12-23 16:20:20 +0900

    fix: Login And Signup Fix/2024-12-23

--- src/user/pages/signup/pages/Signup.js
+++ src/user/pages/signup/pages/Signup.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import '../css/Signup.css';
 import '../css/reset.css';
 
@@ -17,8 +17,6 @@ function Signup() {
     const [showPassword, setShowPassword] = useState(false);
     const [loading, setLoading] = useState(false); // 이메일 다음 버튼 때문에
     const [emailPending, setEmailPending] = useState(false);  // 이메일 대기
-    const termsContentRef = useRef(null);
-    const [isTermsRead, setIsTermsRead] = useState(false)
 
     const handleChange = (e) => {
         const { name, value } = e.target;
