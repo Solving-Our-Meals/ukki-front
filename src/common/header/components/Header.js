adb1de1f30a65700ab1f901092a211d59285d548
Pear1yCode <gh_wns@naver.com>
2024-12-31 11:43:45 +0900

    feat: Add Logout/2024-12-31

--- src/common/header/components/Header.js
+++ src/common/header/components/Header.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 import headerLogo from '../images/header-logo.png';
 import menuIcon from '../images/menu-icon.png'; // 햄버거 아이콘 추가
 import '../css/reset.css';
@@ -7,9 +7,42 @@ import '../css/header.css';
 
 function Header() {
     const [menuOpen, setMenuOpen] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
 
     const toggleMenu = () => {
         setMenuOpen(!menuOpen);
+    };
+
+    const getCookie = (name) => {
+        const value = `; ${document.cookie}`;
+        const parts = value.split(`; ${name}=`);
+        if (parts.length === 2) return parts.pop().split(';').shift();
+        return null;
+    };
+
+    useEffect(() => {
+        const authToken = getCookie('authToken');
+        if (authToken) {
+            setIsLoggedIn(true);
+        } else {
+            setIsLoggedIn(false);
+        }
+    }, []);
+
+    const handleLogout = async () => {
+        // 서버에 로그아웃 요청 보내기
+        await fetch('/auth/logout', {
+            method: 'POST', // 로그아웃 요청은 POST 방식으로 보낼 수 있습니다.
+            credentials: 'include', // 쿠키를 서버에 함께 전송
+        });
+
+        // 로그아웃 후 로그인 상태를 false로 변경
+        setIsLoggedIn(false);
+
+        // 로그인 페이지로 리다이렉트 (또는 원하는 페이지로 이동)
+        navigate('/main');
+        window.location.reload();
     };
 
     return (
@@ -21,8 +54,14 @@ function Header() {
                     <NavLink to="/info">소개</NavLink>
                     <NavLink to="/">공지사항</NavLink>
                     <NavLink to="/">마이페이지</NavLink>
-                    <NavLink to="/auth/login">로그인</NavLink>
-                    <NavLink to="/auth/signup">회원가입</NavLink>
+                    {!isLoggedIn ? (
+                        <>
+                            <NavLink to="/auth/login">로그인</NavLink>
+                            <NavLink to="/auth/signup">회원가입</NavLink>
+                        </>
+                    ) : (
+                        <NavLink to="#" onClick={handleLogout} className="userLogout">로그아웃</NavLink>
+                    )}
                 </span>
                 <button className="menu-button" onClick={toggleMenu}>
                     <img src={menuIcon} alt="Menu" />
