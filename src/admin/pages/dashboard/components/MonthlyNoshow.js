45f58bdebd58baafe73092a94a57fc7bded6138c
Passbob <gudjtr097@gmail.com>
2025-01-01 00:15:19 +0900

    feat: Admin Dashboard Complete/2025-01-01

--- src/admin/pages/dashboard/components/MonthlyNoshow.js
+++ src/admin/pages/dashboard/components/MonthlyNoshow.js
@@ -6,18 +6,14 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 
 export default function MonthlyNoshow(data){
 
-    console.log('hi')
-
     const filterData = Object.values(data);
     const dataValues = Object.values(filterData[0]);
 
-    console.log(dataValues)
-
     const datas = {
-        labels: ['월간 예약', '월간 노쇼'],
+        labels: ['월간 확인 예약', '월간 노쇼'],
         datasets: [
             {
-                data: [dataValues[0], dataValues[1]],
+                data: [dataValues[0]-dataValues[1], dataValues[1]],
                 backgroundColor: ['#FEDA00', '#FF5D18'],
                 borderWidth: 0,
             }
@@ -37,8 +33,11 @@ export default function MonthlyNoshow(data){
     };
 
     return (
+        <>
+        <div id="montlyNoshowContent">{`총 ${dataValues[0]}건 중 `}<p>{dataValues[1]}</p>건</div>
         <div id='montlyNoshowCount'>
         <Pie data={datas} options={Options}></Pie>
         </div>
+        </>
     );
 }
