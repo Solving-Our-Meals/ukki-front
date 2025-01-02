b80032ecd9d6aaa7c435110a9fb5e12bc475783c
DESKTOP-43RUNA1\USER <rhkdlf48301@gmail.com>
2025-01-03 00:00:56 +0900

    feature: storedetail/2025-01-03

--- src/user/pages/mypage/pages/MypageMain.js
+++ src/user/pages/mypage/pages/MypageMain.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import styles from '../css/MypageMain.module.css'
-import MyProfile from "./MyProfile";
-import '../css/reset.css';
+// import React, { useState } from 'react';
+// import styles from '../css/MypageMain.module.css'
+// import MyProfile from "./MyProfile";
+// import '../css/reset.css';
 
-function MypageMain () {
-    const [reservation, setReservation] = useState([]);
+// function MypageMain () {
+//     const [reservation, setReservation] = useState([]);
 
-    return (
-        <div className={styles.mypageMain}>
-            <div className={styles.contentList}></div>
-            <MyProfile />
-        </div>
-    );
-}
+//     return (
+//         <div className={styles.mypageMain}>
+//             <div className={styles.contentList}></div>
+//             <MyProfile />
+//         </div>
+//     );
+// }
 
-export default MypageMain;
+// export default MypageMain;
