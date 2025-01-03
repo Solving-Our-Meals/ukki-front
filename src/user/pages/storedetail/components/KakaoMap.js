c0f507d1d159be03c440b406d4e16cccbbb47a00
JUHYE0925 <rhkdlf48301@gmail.com>
2025-01-03 15:36:28 +0900

    feature: storedetail/2025-01-03

--- src/user/pages/storedetail/components/KakaoMap.js
+++ src/user/pages/storedetail/components/KakaoMap.js
@@ -1,135 +1,5 @@
-import { CustomOverlayMap, Map, MapMarker } from "react-kakao-maps-sdk";
-import { useState, useEffect } from 'react';
-import marMarker from '../images/smallMapMarker-logo.png';
-
-function KakaoMap(){
-    const [ storeLatitude, setStoreLatitude ] = useState("")
-    const [ storeLongitude, setStoreLongitude ] = useState("")
-    const [ storeName, setStoreName ] = useState("")
-
-    // 지도에서 길찾기/큰지도 보기 할 때 해당 가게의 위도/경도를 링크로 넘겨 카카오맵에서 보여지게 하기.
-    const mapLink  = `https://map.kakao.com/link/map/${storeName},${storeLatitude},${storeLongitude}`
-
-    useEffect(() => {
-        fetch('/store/test')
-        .then(res => res.json())
-        .then(data => {
-            setStoreLatitude(data.latitude);
-            setStoreLongitude(data.longitude);
-            setStoreName(data.storeName);
-        });
-    }, [])
-    
-    return(
-        <>
-            <Map
-                // 지도로 보여줄 위치 지정(위도, 경도)
-                center={{ lat : storeLatitude, lng : storeLongitude}}
-                // 지도 스타일 적용
-                style = {{
-                    position : 'absolute',
-                    left : '1064px',
-                    top : '585px',
-                    width: '698px',
-                    height : '352px',
-                    border : '7.21px solid #FFA8B8',
-                    borderRadius : '36px'
-                }}
-                level={3}
-            >
-                <MapMarker 
-                    //  핀 찍힐 위치 지정
-                    style={{ border : 'transparent' }}
-                    position={{ lat : storeLatitude, lng : storeLongitude }}
-                    // 마커 커시텀할 이미지 주소 및 스타일 적용
-                    image={{ 
-                        src : marMarker,
-                        size : {
-                            width : 64,
-                            height : 85
-                        },
-                        // 마커의 포인터가 놓일 위치
-                        // 마커의 크기가 width : 64, height : 85일 경우 
-                        // 포인터의 위치는 width의 중간, height의 끝에 와야하기에 다음과 같이 적용
-                        options : {
-                            offset : {
-                                x : 32,
-                                y : 85,
-                            },
-                        },
-                        }}
-                >
-                    <CustomOverlayMap
-                        position={{ lat : storeLatitude, lng : storeLongitude}}
-                        yAnchor={2.5}
-                    >
-                        <div className="customoverlay"
-                                style = {{
-                                color : '#323232',
-                                fontFamily : 'Pretendard Variable',
-                                fontSize : '17px',
-                                fontStyle : 'Regular',
-                                fontWeight : 600,
-                                display : "block",
-                                flexDirection : "column",
-                                border : '2px solid #323232',
-                                borderRadius : '36px',
-                                backgroundColor : '#FFFFFF',
-                                padding : 10,
-                                textAlign : 'center'
-                                }}   
-                        >
-                            <div>
-                                {storeName}
-                            </div>
-                            <div>
-                                <a
-                                    href= {mapLink}
-                                    style={{
-                                        color : '#007AFF',
-                                        fontFamily : 'Pretendard Variable',
-                                        fontSize : '15px',
-                                        fontStyle : 'Regular',
-                                        border : 'none'
-                                    }}
-                                    // <a> 속성 
-                                    target="_blank"  // 링크를 새 탭 또는 새 창에서 열리게 함.
-                                    // rel = "noreferrer"
-                                    // 링크를 열 때 링크된 페이지에 참조 정보 제공하지 않는다. 
-                                    // 이는 보안과 개인정보 보호를 위해 링크된 페이지에 HTTP Referrer 헤더를 보내지 않는다. 
-                                    // 즉 링크된 페이지가 사용자가 어떤 페이지에서 왔는지 알지 못하게 한다.
-                                    rel="noreferrer" 
-                                    // 위 두 속성을 사용하면 보안을 강화하고 개인정보를 보호하는데 도움이 된다.
-                                >
-                                    &emsp; 큰지도보기 &nbsp;
-                                </a>{" "}
-                                <a
-                                    href={mapLink}
-                                    style={{
-                                        color : '#007AFF',
-                                        fontFamily : 'Pretendard Variable',
-                                        fontSize : '15px',
-                                        fontStyle : 'Regular',
-                                    }}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                >
-                                   &nbsp; 길찾기 &emsp;
-                                </a>
-                            </div>
-                        </div>
-                    </CustomOverlayMap>
-                </MapMarker>
-            </Map>
-        </>
-    );
-}
-
-export default KakaoMap;
-
 // import { CustomOverlayMap, Map, MapMarker } from "react-kakao-maps-sdk";
 // import { useState, useEffect } from 'react';
-// import styles from '../css/kakaoMap.module.css'
 // import marMarker from '../images/smallMapMarker-logo.png';
 
 // function KakaoMap(){
@@ -156,7 +26,15 @@ export default KakaoMap;
 //                 // 지도로 보여줄 위치 지정(위도, 경도)
 //                 center={{ lat : storeLatitude, lng : storeLongitude}}
 //                 // 지도 스타일 적용
-//                 id={styles.mapContainer}
+//                 style = {{
+//                     position : 'absolute',
+//                     left : '1064px',
+//                     top : '585px',
+//                     width: '698px',
+//                     height : '352px',
+//                     border : '7.21px solid #FFA8B8',
+//                     borderRadius : '36px'
+//                 }}
 //                 level={3}
 //             >
 //                 <MapMarker 
@@ -185,14 +63,35 @@ export default KakaoMap;
 //                         position={{ lat : storeLatitude, lng : storeLongitude}}
 //                         yAnchor={2.5}
 //                     >
-//                         <div className={styles.customoverlay}>
+//                         <div className="customoverlay"
+//                                 style = {{
+//                                 color : '#323232',
+//                                 fontFamily : 'Pretendard Variable',
+//                                 fontSize : '17px',
+//                                 fontStyle : 'Regular',
+//                                 fontWeight : 600,
+//                                 display : "block",
+//                                 flexDirection : "column",
+//                                 border : '2px solid #323232',
+//                                 borderRadius : '36px',
+//                                 backgroundColor : '#FFFFFF',
+//                                 padding : 10,
+//                                 textAlign : 'center'
+//                                 }}   
+//                         >
 //                             <div>
 //                                 {storeName}
 //                             </div>
 //                             <div>
 //                                 <a
 //                                     href= {mapLink}
-//                                     className={styles.mapLink}
+//                                     style={{
+//                                         color : '#007AFF',
+//                                         fontFamily : 'Pretendard Variable',
+//                                         fontSize : '15px',
+//                                         fontStyle : 'Regular',
+//                                         border : 'none'
+//                                     }}
 //                                     // <a> 속성 
 //                                     target="_blank"  // 링크를 새 탭 또는 새 창에서 열리게 함.
 //                                     // rel = "noreferrer"
@@ -206,7 +105,12 @@ export default KakaoMap;
 //                                 </a>{" "}
 //                                 <a
 //                                     href={mapLink}
-//                                     className={styles.mapLink}
+//                                     style={{
+//                                         color : '#007AFF',
+//                                         fontFamily : 'Pretendard Variable',
+//                                         fontSize : '15px',
+//                                         fontStyle : 'Regular',
+//                                     }}
 //                                     target="_blank"
 //                                     rel="noreferrer"
 //                                 >
@@ -222,3 +126,141 @@ export default KakaoMap;
 // }
 
 // export default KakaoMap;
+
+import { CustomOverlayMap, Map, MapMarker } from "react-kakao-maps-sdk";
+import { useState, useEffect } from 'react';
+import styles from '../css/kakaoMap.module.css'
+import mapMarker from '../images/smallMapMarker-logo.png';
+
+function KakaoMap(){
+    const [ storeLatitude, setStoreLatitude ] = useState("")
+    const [ storeLongitude, setStoreLongitude ] = useState("")
+    const [ storeName, setStoreName ] = useState("")
+
+    // 지도에서 길찾기/큰지도 보기 할 때 해당 가게의 위도/경도를 링크로 넘겨 카카오맵에서 보여지게 하기.
+    const mapLink  = `https://map.kakao.com/link/map/${storeName},${storeLatitude},${storeLongitude}`
+
+    useEffect(() => {
+        fetch('/store/test')
+        .then(res => res.json())
+        .then(data => {
+            setStoreLatitude(data.latitude);
+            setStoreLongitude(data.longitude);
+            setStoreName(data.storeName);
+        });
+    }, [])
+
+    // 반응형
+    const [markerStyle, setMarkerStyle] = useState({
+        width : 64,
+        height : 85,
+        offsetX : 32,
+        offsetY : 85
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            const width = Window.innerWidth;
+
+            if(width <= 768){
+                setMarkerStyle({
+                    width : 32,
+                    height : 42.5,
+                    offsetX : 16,
+                    offsetY : 42.5,
+                });
+            } else if (width <= 1024 && width > 768){
+                setMarkerStyle({
+                    width : 48,
+                    height : 63.75,
+                    offsetX : 24,
+                    offsetY : 63.75,
+                });
+            } else {
+                setMarkerStyle({
+                    width : 64,
+                    height : 85,
+                    offsetX : 32,
+                    offsetY : 85,
+                });
+            }
+        }
+        window.addEventListener('resize', handleResize);
+        handleResize(); // 초기 로드 시에도 한 번 실행
+
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+    
+    return(
+        <>
+            <Map
+                // 지도로 보여줄 위치 지정(위도, 경도)
+                center={{ lat : storeLatitude, lng : storeLongitude}}
+                // 지도 스타일 적용
+                id={styles.mapContainer}
+                level={3}
+            >
+                <MapMarker 
+                    //  핀 찍힐 위치 지정
+                    style={{ border : 'transparent' }}
+                    className={styles.mapmarker}
+                    position={{ lat : storeLatitude, lng : storeLongitude }}
+                    // 마커 커시텀할 이미지 주소 및 스타일 적용
+                    image={{ 
+                        src : mapMarker,
+                        size : {
+                            width : markerStyle.width,
+                            height : markerStyle.height,
+                        },
+                        // 마커의 포인터가 놓일 위치
+                        // 마커의 크기가 width : 64, height : 85일 경우 
+                        // 포인터의 위치는 width의 중간, height의 끝에 와야하기에 다음과 같이 적용
+                        options : {
+                            offset : {
+                                x : markerStyle.offsetX,
+                                y : markerStyle.offsetY,
+                            },
+                        },
+                    }}
+                >
+                    <CustomOverlayMap
+                        position={{ lat : storeLatitude, lng : storeLongitude}}
+                        yAnchor={2.29}
+                    >
+                        <div className={styles.customoverlay}>
+                            <div>
+                                {storeName}
+                            </div>
+                            <div>
+                                <a
+                                    href= {mapLink}
+                                    className={styles.mapLink}
+                                    // <a> 속성 
+                                    target="_blank"  // 링크를 새 탭 또는 새 창에서 열리게 함.
+                                    // rel = "noreferrer"
+                                    // 링크를 열 때 링크된 페이지에 참조 정보 제공하지 않는다. 
+                                    // 이는 보안과 개인정보 보호를 위해 링크된 페이지에 HTTP Referrer 헤더를 보내지 않는다. 
+                                    // 즉 링크된 페이지가 사용자가 어떤 페이지에서 왔는지 알지 못하게 한다.
+                                    rel="noreferrer" 
+                                    // 위 두 속성을 사용하면 보안을 강화하고 개인정보를 보호하는데 도움이 된다.
+                                >
+                                    &emsp; 큰지도보기 &nbsp;
+                                </a>{" "}
+                                <a
+                                    href={mapLink}
+                                    className={styles.mapLink}
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                   &nbsp; 길찾기 &emsp;
+                                </a>
+                            </div>
+                        </div>
+                    </CustomOverlayMap>
+                </MapMarker>
+            </Map>
+        </>
+    );
+}
+
+export default KakaoMap;
--- src/user/pages/storedetail/pages/Review.js
+++ src/user/pages/storedetail/pages/Review.js
@@ -1,3 +1,84 @@
+// import { useState, useEffect } from 'react';
+// import styles from '../css/review.module.css';
+// import reviewImg from '../images/reviewBackground.png';
+// import CreateReview from '../components/CreateReview';
+
+// function Review(){
+
+//     const [reviews, setReviews] = useState([]);
+//     const [reviewContent, setReviewContent] = useState({
+//         storeNo : "",
+//         reviewCount : 0,
+//         reviewList : []
+//     });
+//     const [isMoreReview, setIsMoreReview] = useState(false);
+//     const [hiddenWord, setHiddenWord] = useState(true);
+
+//     useEffect(
+//         () => {
+//             fetch('/store/review')
+//             .then(res => res.json())
+//             .then(data => {
+//                 console.log('리뷰 정보 : ', data);
+//                 setReviewContent(data);
+//                 const reviewList = data.reviewList;
+//                 setReviews(reviewList);
+//             })
+//             .catch(error => console.log(error));
+//         }, []
+//     );
+
+//     const moreReviewHandler = () => {
+//         setIsMoreReview(prevState => !prevState);
+//         setHiddenWord(prevState => !prevState);
+//     }
+    
+//     return(
+//         <div className={styles.reviewStyle}>
+//             <div id={styles.strReview}>리뷰</div>
+//             <div id={styles.strCountReview}>{`총 ${reviewContent.reviewCount}개의 리뷰가 있습니다.`}</div>
+//             <div className={styles.reviewArea}>
+//                 <div className={styles.reviewContainer}>
+//                     {reviews.map((review, index) => (
+//                         <div 
+//                             key={index} 
+//                             className={styles.review} 
+//                             style={{ 
+//                                 marginLeft : index % 2 !== 0 ? '35px' : '-35px', 
+//                                 display : isMoreReview || index <= 2 ? "" : "none" }}
+//                         >
+//                             <img src={reviewImg} id={styles.reviewImg} style={{ transform : index % 2 !== 0 ? 'scaleX(-1)' : "" }} alt='리뷰 백그라운드'/>
+//                             <img 
+//                                 src={review.userProfile === null ? '/store/api/userProfile?userProfileName=PROFILE_BASIC' : `/store/api/userProfile?userProfileName=${review.userProfile}`} 
+//                                 id={styles.userProfile} 
+//                                 style={{ 
+//                                     left : index % 2 !== 0 ? '613px' : "" , 
+//                                     top : index % 2 !== 0 ? '53px' : ""
+//                                     }}
+//                                 alt='프로필 이미지'
+//                                 />
+//                             <div style={{ left : index % 2 !== 0 ? '744px' : "" , top : index % 2 !== 0 ? '70px' : ""}}>{review.userName}</div>
+//                             {/* 예약 시간이 DB에서 데이터가 서로 안맞음. 그래서 리뷰 작성 날짜로 변경할지 DB를 바꿀지 고민 중 */}
+//                             {/* <div style={{ left : index % 2 !== 0 ? '747px' : "" , top : index % 2 !== 0 ? '111px' : ""}}>{`예약시간 - ${review.reviewDate}`}</div> */}
+//                             <div style={{ left : index % 2 !== 0 ? '747px' : "" , top : index % 2 !== 0 ? '111px' : ""}}>{review.reviewDate}</div>
+//                             <div style={{ left : index % 2 !== 0 ? '624px' : "" , top : index % 2 !== 0 ? '176px' : ""}}>{review.reviewContent}</div>
+//                             <img src={`/store/api/reviewImg?reviewImgName=${review.reviewImage}`} id={styles.reviewPhoto} style={{ left : index % 2 !== 0 ? '43px' : "" , top : index % 2 !== 0 ? '38px' : ""}} alt='리뷰 사진'/>
+//                         </div>
+//                     ))}
+//                 </div>
+//                 <div 
+//                     id={styles.moreReview} 
+//                     onClick={() => moreReviewHandler()}
+//                 >
+//                     {hiddenWord ? "리뷰 더보기 > " : "리뷰 닫기"} 
+//                 </div>
+//             </div>
+//             <CreateReview/>
+//         </div>
+//     );
+// }
+
+// export default Review;
 import { useState, useEffect } from 'react';
 import styles from '../css/review.module.css';
 import reviewImg from '../images/reviewBackground.png';
@@ -42,27 +123,20 @@ function Review(){
                     {reviews.map((review, index) => (
                         <div 
                             key={index} 
-                            className={styles.review} 
+                            className={ index % 2 !== 0 ? styles.transformReview : styles.review} 
                             style={{ 
-                                marginLeft : index % 2 !== 0 ? '35px' : '-35px', 
                                 display : isMoreReview || index <= 2 ? "" : "none" }}
                         >
                             <img src={reviewImg} id={styles.reviewImg} style={{ transform : index % 2 !== 0 ? 'scaleX(-1)' : "" }} alt='리뷰 백그라운드'/>
                             <img 
                                 src={review.userProfile === null ? '/store/api/userProfile?userProfileName=PROFILE_BASIC' : `/store/api/userProfile?userProfileName=${review.userProfile}`} 
                                 id={styles.userProfile} 
-                                style={{ 
-                                    left : index % 2 !== 0 ? '613px' : "" , 
-                                    top : index % 2 !== 0 ? '53px' : ""
-                                    }}
                                 alt='프로필 이미지'
                                 />
-                            <div style={{ left : index % 2 !== 0 ? '744px' : "" , top : index % 2 !== 0 ? '70px' : ""}}>{review.userName}</div>
-                            {/* 예약 시간이 DB에서 데이터가 서로 안맞음. 그래서 리뷰 작성 날짜로 변경할지 DB를 바꿀지 고민 중 */}
-                            {/* <div style={{ left : index % 2 !== 0 ? '747px' : "" , top : index % 2 !== 0 ? '111px' : ""}}>{`예약시간 - ${review.reviewDate}`}</div> */}
-                            <div style={{ left : index % 2 !== 0 ? '747px' : "" , top : index % 2 !== 0 ? '111px' : ""}}>{review.reviewDate}</div>
-                            <div style={{ left : index % 2 !== 0 ? '624px' : "" , top : index % 2 !== 0 ? '176px' : ""}}>{review.reviewContent}</div>
-                            <img src={`/store/api/reviewImg?reviewImgName=${review.reviewImage}`} id={styles.reviewPhoto} style={{ left : index % 2 !== 0 ? '43px' : "" , top : index % 2 !== 0 ? '38px' : ""}} alt='리뷰 사진'/>
+                            <div>{review.userName}</div>
+                            <div>{review.reviewDate}</div>
+                            <div>{review.reviewContent}</div>
+                            <img src={`/store/api/reviewImg?reviewImgName=${review.reviewImage}`} id={styles.reviewPhoto} alt='리뷰 사진'/>
                         </div>
                     ))}
                 </div>
