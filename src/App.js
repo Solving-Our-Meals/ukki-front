3451478a25820e633379952f64942707dda6c4e8
Pear1yCode <gh_wns@naver.com>
2025-01-03 15:33:53 +0900

    fix: Fix RefreshToken I Finally Fixed it../2025-01-03

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import { AuthProvider, useAuth } from './common/authContext/AuthContext';
 
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
-import { useEffect } from 'react';
-import Signup from '../src/user/pages/signup/pages/Signup';
-import Login from '../src/user/pages/login/pages/Login'
-import Find from '../src/user/pages/find/pages/Find'
-import Mypage from '../src/user/pages/mypage/component/Mypage'
+import Signup from './user/pages/signup/pages/Signup';
+import Login from './user/pages/login/pages/Login';
+import Find from './user/pages/find/pages/Find';
+import Mypage from './user/pages/mypage/component/Mypage';
 import Info from './user/pages/info/Info';
 import UserLayout from './user/layouts/UserLayout';
 import Main from './user/pages/main/Main';
@@ -13,46 +14,30 @@ import UserStorePage from './user/pages/storedetail/pages/UserStorePage';
 import QrRoutes from './store/pages/qrCheck/routes/QrRoutes';
 import AdminRoutes from './admin/route/AdminRoutes';
 import Search from './user/pages/search/Search';
-import useAuth from './common/authContext/useAuth'
 
 function App() {
-  const { authToken, refreshAuthToken } = useAuth();
-
-  useEffect(() => {
-    if (!authToken) {
-      refreshAuthToken();
-    }
-  }, [authToken, refreshAuthToken]);
-
-  const ProtectedRoute = ({ children }) => {
-    if (!authToken) {
-      return <Navigate to="/auth/login" />; // 인증되지 않으면 로그인 페이지로 리디렉션
-    }
-
-    return children;
-  };
 
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="auth/signup" element={<Signup/>}/>
-      <Route path="auth/login" element={<Login/>}/>
-      <Route path="auth/find/:type" element={<Find/>}/>
-      <Route element={<ProtectedRoute />}>
-      <Route path="user/mypage" element={<Mypage/>}/>
-      <Route path="/" element={<UserLayout/>}>
-      <Route index element={<Main/>}/>
-      <Route path="search" element={<Search/>}/>
-      <Route path="info" element={<Info/>}/>
-      <Route path="main" element={<Main/>}/>
-      <Route path="store" element={<UserStorePage/>}/>
-      <Route path="sinquiries" element={<InquiryEnter/>}/>
-      </Route>
-      <Route path="qr/*" element={<QrRoutes/>}/>
-      <Route path='admin/*' element={<AdminRoutes/>}/>
-      </Route>
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="auth/signup" element={<Signup />} />
+          <Route path="auth/login" element={<Login />} />
+          <Route path="auth/find/:type" element={<Find />} />
+          <Route path="user/mypage" element={<Mypage />} />
+          <Route path="/" element={<UserLayout />}>
+            <Route index element={<Main />} />
+            <Route path="search" element={<Search />} />
+            <Route path="info" element={<Info />} />
+            <Route path="main" element={<Main />} />
+            <Route path="store" element={<UserStorePage />} />
+            <Route path="sinquiries" element={<InquiryEnter />} />
+          </Route>
+          <Route path="qr/*" element={<QrRoutes />} />
+          <Route path="admin/*" element={<AdminRoutes />} />
+        </Routes>
+        </AuthProvider>
+      </BrowserRouter>
   );
 }
 
