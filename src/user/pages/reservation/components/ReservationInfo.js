ff1d86def0f85fd2d1f90286c8333767980ee5aa
DESKTOP-43RUNA1\USER <rhkdlf48301@gmail.com>
2025-01-06 01:57:45 +0900

    feature: reservation/2025-01-06

--- src/user/pages/reservation/components/ReservationInfo.js
+++ src/user/pages/reservation/components/ReservationInfo.js
@@ -1,18 +1,32 @@
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import styles from '../css/reservationInfo.module.css';
 import KakaoMap from '../components/KakaoMap';
 
 function ReservationInfo(){
 
+    const [userInfo, setUserInfo] = useState({});
     const location = useLocation();
 
     const reservationInfo = { ...location.state };
+
+    useEffect(
+        () => {
+            fetch('user/info')
+            .then(res => res.json())
+            .then(data => {
+                setUserInfo(data);
+                console.log('유저정보', data);
+            })
+            .then(error => console.log(error));
+        }, []
+    )
 
     return(
         <div className={styles.reservationInfo}>
             <div id={styles.orderer}>
                 <p>주문자 정보</p>
-                <p>닉네임 :</p>
+                <p>{`닉네임 : ${userInfo.nickname}`}</p>
             </div>
             <div id={styles.reservationConfirm}>
                 <p>예약 정보 확인</p>
