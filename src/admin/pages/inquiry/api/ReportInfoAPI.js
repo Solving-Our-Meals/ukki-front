b2bef82c038e51c28bbc4b65d2f6519037204462
Passbob <gudjtr097@gmail.com>
2025-01-16 12:41:34 +0900

    feat : Admin Inquiry Info Edit Delete/2025-01-16

--- src/admin/pages/inquiry/api/ReportInfoAPI.js
+++ src/admin/pages/inquiry/api/ReportInfoAPI.js
@@ -1,5 +1,7 @@
 export async function ReportInfoAPI(reportNo){
+    console.log(reportNo);
     const res = await fetch(`/admin/inquiries/info/report/${reportNo}`)
     const reportInfo = await res.json();
+    console.log(reportInfo);
     return reportInfo
 } 
