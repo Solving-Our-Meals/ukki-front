5d48fc009a1190cb71a4ab43f6b25a7053f09ee8
Passbob <gudjtr097@gmail.com>
2025-01-27 11:53:26 +0900

    merge: EC2 Test /2025-01-27

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,2 +1,3 @@
 // export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
-export const API_BASE_URL = '/api';
+export const API_BASE_URL = '';  // 빈 문자열로 설정
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://backend:8080';
