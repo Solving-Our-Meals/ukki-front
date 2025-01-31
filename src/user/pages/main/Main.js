7f9740b3fad9342aec5695cfc56326cb930262b9
Passbob <gudjtr097@gmail.com>
2025-01-31 20:16:07 +0900

    Fix Main.js
    useNavigate가 2번 정의되어서 에러가났습니다.

--- src/user/pages/main/Main.js
+++ src/user/pages/main/Main.js
@@ -335,8 +335,6 @@ const Main = () => {
 
     // };
 
-    const navigate = useNavigate(); // useNavigate 훅 사용
-
     const handleReservationClick = () => {
         if (storeInfo && storeInfo.storeNo) {
             console.log("Selected StoreNo: ", storeInfo.storeNo);  // 디버깅용
