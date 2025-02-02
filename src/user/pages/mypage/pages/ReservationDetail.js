2c94c842f42818d839243e83f6ca1c261074740e
Pear1yCode <gh_wns@naver.com>
2025-02-02 21:46:35 +0900

    fix: For Complement/2025-02-02

--- src/user/pages/mypage/pages/ReservationDetail.js
+++ src/user/pages/mypage/pages/ReservationDetail.js
@@ -11,7 +11,6 @@ function ReservationDetail() {
     const [calendarVisible, setCalendarVisible] = useState(false); // 달력 보이기 상태 추가
     const [error, setError] = useState(null);
     const [qrImage, setQrImage] = useState('');
-    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         fetchReservationDetail();
