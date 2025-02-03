9d4a3ef850f826286b3ff9acd74ceaa3254cffe5
Pear1yCode <gh_wns@naver.com>
2025-02-03 12:10:38 +0900

    fix: For Complement/2025-02-03

--- src/user/pages/mypage/pages/ReviewDetail.js
+++ src/user/pages/mypage/pages/ReviewDetail.js
@@ -161,15 +161,24 @@ function ReviewDetail() {
                 <div className={styles.reviewText}>{reviewDetail.reviewText}</div>
 
                 {/* 리뷰 사진 표시 */}
-                {reviewDetail.reviewPicture && (
+                {reviewDetail.reviewPicture ? (
                     <div className={styles.reviewImageContainer}>
                         <img
-                            src={reviewImage || DefaultReview}
+                            src={reviewDetail.reviewPicture}
+                            alt="리뷰 이미지"
+                            className={styles.reviewImage}
+                        />
+                    </div>
+                ) : (
+                    <div className={styles.reviewImageContainer}>
+                        <img
+                            src={DefaultReview}
                             alt="리뷰 이미지"
                             className={styles.reviewImage}
                         />
                     </div>
                 )}
+
             </div>
         </>
     );
